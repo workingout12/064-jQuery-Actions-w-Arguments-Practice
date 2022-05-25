a05169e6cc3f38830a7d7c212ea6480e99592044
workingout12 <106279253+workingout12@users.noreply.github.com>
2022-05-25 16:46:18 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homer-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice (START YOUR CODE BELOW!)
+$(".reply-no").click(function() {  
+        $(".homer-reply").text("Doh");
+    $(".reply-yes").css("background-color", "pink");
+});
+
